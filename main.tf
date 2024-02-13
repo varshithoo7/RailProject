17410e75b85580b02619964f7d4651871375a2aa
svarshith7 <svarshith@gmail.com>
2024-02-13 13:14:48 -0500

    Terraform SSH and File Block Paths updated to Local

--- main.tf
+++ main.tf
@@ -24,19 +24,19 @@ resource "aws_instance" "AutomationInstance" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = file("${path.module}/Kp.pem")
+      private_key = file("C:/Users/SuperMan/Desktop/Kp.pem")
       host        = self.public_ip
     }
   }
 
   provisioner "file" {
-    source      = "${path.module}"
+    source      = "C:/Users/SuperMan/Desktop/Railway-Management-master/*"
     destination = "/home/ec2-user/RailProject"
     
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = file("${path.module}/Kp.pem")
+      private_key = file("C:/Users/SuperMan/Desktop/Kp.pem")
       host        = self.public_ip
     }
   }
